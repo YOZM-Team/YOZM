4f588ca68746a5bdc4b3b3b6935e12857f9b044d
heejinnn <gml0593@gmail.com>
2025-08-09 22:29:51 +0900

    chore: xcode 프로젝트 생성

---
+++ YOZM/YOZM/YOZMApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOZMApp.swift
+//  YOZM
+//
+//  Created by 최희진 on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct YOZMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
