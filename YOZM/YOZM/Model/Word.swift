992afe60c46f13056aecd5f4d50c9602a1636e5e
Huigyun Jeong <topone03@gmail.com>
2025-08-30 11:28:49 +0900

    feat: add DialogueSentence model and update Word to use DialogueSentence for sampleDialogue

--- YOZM/YOZM/Model/Word.swift
+++ YOZM/YOZM/Model/Word.swift
@@ -12,5 +12,21 @@ struct Word {
     let word: String
     let meaning: String
     let sampleSentence: String
-    let sampleDialogue: [String]
+    let sampleDialogue: [DialogueSentence]
+}
+
+extension Word {
+    static var sampleWord: Word {
+        Word(
+            id: UUID(),
+            word: "느좋",
+            meaning: "느낌 좋은, 어떤 대상이나 상황에 대해 긍정적인 느낌, 즉 좋다는 감정을 표현할 때 사용",
+            sampleSentence: "느좋 카페 갈래?",
+            sampleDialogue: [
+                DialogueSentence(index: 0, speaker: .other, sentence: "야, 느좋 카페 갈래?"),
+                DialogueSentence(index: 1, speaker: .me, sentence: "아니, 사람 많아서 싫어"),
+                DialogueSentence(index: 2, speaker: .other, sentence: "아 왜 그냥 가자"),
+            ]
+        )
+    }
 }
