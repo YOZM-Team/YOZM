7acd7a6815544835b5b2b82c24e8bd754d2a01c2
heejinnn <gml0593@gmail.com>
2025-09-06 14:35:35 +0900

    feat: add MainTabView, MyPageView, CollectionView

---
+++ YOZM/YOZM/View/MyPage/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  YOZM
+//
+//  Created by 최희진 on 9/6/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
